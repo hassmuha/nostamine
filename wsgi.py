716df8e2aedf8ddaf8e4a1a374ba2399c1a926ac
Muhammad Hassan <hassan.nust@gmail.com>
2017-01-16 00:17:34 +0100

    Tt

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,9 @@
-from flask import Flask, request
-import json
-import requests
+from flask import Flask
+application = Flask(__name__)
 
-app = Flask(__name__)
-
-# This needs to be filled with the Page Access Token that will be provided
-# by the Facebook App that will be created.
-#PAT = 'EAAIeJYNmvk0BACXjV9sUcwwNnfg0EM2y5zv2prZAH6ilxX9ouAHZBM1ZC9Hn96cUSVRCtK5fXuo1qnbZAMZC0jysfdhURw5Kq6VmB0g80AX9LpZCF7Ro0NcOXZCR4ZBCfvAsGU4aeRJD8mZBaGhBzZB00x5bbOZAluuS7IelpZAOTPbq1AZDZD'
-
-
-@app.route("/")
+@application.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
