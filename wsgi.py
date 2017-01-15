8d30d79c2b358733b5dc24378dd0ba7c23c4e8d1
Muhammad Hassan <hassan.nust@gmail.com>
2017-01-16 00:27:00 +0100

    only get method

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,27 @@ import requests
 
 application = Flask(__name__)
 
+# This needs to be filled with the Page Access Token that will be provided
+# by the Facebook App that will be created.
+PAT = 'EAAIeJYNmvk0BACXjV9sUcwwNnfg0EM2y5zv2prZAH6ilxX9ouAHZBM1ZC9Hn96cUSVRCtK5fXuo1qnbZAMZC0jysfdhURw5Kq6VmB0g80AX9LpZCF7Ro0NcOXZCR4ZBCfvAsGU4aeRJD8mZBaGhBzZB00x5bbOZAluuS7IelpZAOTPbq1AZDZD'
+
+
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+
+@application.route('/', methods=['GET'])
+def handle_verification():
+  print "Handling Verification."
+  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+    print "Verification successful!"
+    return request.args.get('hub.challenge', '')
+  else:
+    print "Verification failed!"
+    return 'Error, wrong validation token'
+
 
 if __name__ == "__main__":
     application.run()
