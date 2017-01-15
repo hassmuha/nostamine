b755c4a1b6dec8472fdb1c2cf864ad829101dc44
Muhammad Hassan <hassan.nust@gmail.com>
2017-01-16 00:48:31 +0100

    deleting hello world

--- wsgi.py
+++ wsgi.py
@@ -9,9 +9,9 @@ application = Flask(__name__)
 PAT = 'EAAIeJYNmvk0BACXjV9sUcwwNnfg0EM2y5zv2prZAH6ilxX9ouAHZBM1ZC9Hn96cUSVRCtK5fXuo1qnbZAMZC0jysfdhURw5Kq6VmB0g80AX9LpZCF7Ro0NcOXZCR4ZBCfvAsGU4aeRJD8mZBaGhBzZB00x5bbOZAluuS7IelpZAOTPbq1AZDZD'
 
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+#@application.route("/")
+#def hello():
+#    return "Hello World!"
 
 
 
