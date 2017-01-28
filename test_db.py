c71c52ee0dc440f533a72b5eee5adc7eebd6d78d
Muhammad Hassan <hassan.nust@gmail.com>
2017-01-28 16:57:02 +0100

    Abc

--- test_db.py
+++ test_db.py
@@ -20,32 +20,9 @@ PAT = 'EAAIeJYNmvk0BACXjV9sUcwwNnfg0EM2y5zv2prZAH6ilxX9ouAHZBM1ZC9Hn96cUSVRCtK5f
 
 
 
-@app.route("/")
-def hello():
-    #db = mongo.db
-    # First we'll add a few songs. Nothing is required to create the songs
-    # collection; it is created automatically when we insert.
-    songs = db['songs']
-    # Note that the insert method can take either an array or a single dict.
-    songs.insert(SEED_DATA)
-
-    # Then we need to give Boyz II Men credit for their contribution to
-    # the hit "One Sweet Day".
-
-    query = {'song': 'One Sweet Day'}
-
-    songs.update(query, {'$set': {'artist': 'Mariah Carey ft. Boyz II Men'}})
-
-    # Finally we run a query which returns all the hits that spent 10 or
-    # more weeks at number 1.
-
-    cursor = songs.find({'weeksAtOne': {'$gte': 10}}).sort('decade', 1)
-
-    for doc in cursor:
-        print ('In the %s, %s by %s topped the charts for %d straight weeks.' %
-               (doc['decade'], doc['song'], doc['artist'], doc['weeksAtOne']))
-
-    return "Hello World!"
+#@app.route("/")
+#def hello():
+#    return "Hello World!"
 
 
 
