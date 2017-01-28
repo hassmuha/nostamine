6861564c8b8175be455de034a80385882c4144fb
Muhammad Hassan <hassan.nust@gmail.com>
2017-01-28 22:58:32 +0100

    aa

--- test_db.py
+++ test_db.py
@@ -14,7 +14,7 @@ client = pymongo.MongoClient(os.environ.get('MONGODB_URI'))
 db = client.get_default_database()
 result = db.posts.delete_many({})
 posts = db['bet']
-timestamp = ''
+timestamp = ""
 # This needs to be filled with the Page Access Token that will be provided
 # by the Facebook App that will be created.
 PAT = 'EAAIeJYNmvk0BACXjV9sUcwwNnfg0EM2y5zv2prZAH6ilxX9ouAHZBM1ZC9Hn96cUSVRCtK5fXuo1qnbZAMZC0jysfdhURw5Kq6VmB0g80AX9LpZCF7Ro0NcOXZCR4ZBCfvAsGU4aeRJD8mZBaGhBzZB00x5bbOZAluuS7IelpZAOTPbq1AZDZD'
