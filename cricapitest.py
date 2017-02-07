f4119f23e2a94fe8188ed98799082fd8ed9ac136
Asif Nadeem <a.nadeem89@gmail.com>
2017-02-07 23:19:45 +0100

    cricapitest

--- cricapitest.py
+++ cricapitest.py
@@ -2,7 +2,9 @@ from pycricbuzz import Cricbuzz
 import json
 
 
-def Oneday(data2):
+def livescore(data2):
+    batsmen= len(data2["batting"]["batsman"])
+    bowlers= len(data2["bowling"]["bowler"])
     if "runs" in data2["bowling"]["score"][0] :
         batteam = "%s %s/%s Overs:%s" %(data2["batting"]["team"],data2["batting"]["score"][0]["runs"],data2["batting"]["score"][0]["wickets"],data2["batting"]["score"][0]["overs"])
         bowlteam = "\n%s %s/%s Overs:%s" %(data2["bowling"]["team"],data2["bowling"]["score"][0]["runs"],data2["bowling"]["score"][0]["wickets"],data2["bowling"]["score"][0]["overs"])
@@ -59,6 +61,15 @@ def Oneday(data2):
         #print bowlerinfo
         cscore = batteam + batsmeninfo + bowlteam + bowlerinfo
         return cscore
+
+def preview(data2):
+    cscore = data2["mchdesc"] + "\n" + data2["status"]
+    return cscore
+
+def complete(data2):
+    cscore = data2["mchdesc"] + "\n" + data2["status"]
+    return cscore
+
 c = Cricbuzz()
 matches = c.matches()
 print json.dumps(matches,indent=4) #for pretty prinitng
@@ -76,16 +87,23 @@ print json.dumps(data,indent=4)
 ##})
 ##print data1
 #print json.dumps(c.livescore(1),indent=4)
-data1= c.scorecard(2)
-data2 = c.livescore(18012)
-matchinfo = data2["matchinfo"]
-scorecard = data1["scorecard"]
-innings= len(scorecard)
-batsmen= len(data2["batting"]["batsman"])
-bowlers= len(data2["bowling"]["bowler"])
+#data1= c.scorecard(2)
+
+#matchinfo = data2["matchinfo"]
+#scorecard = data1["scorecard"]
+#innings= len(scorecard)
+#batsmen= len(data2["batting"]["batsman"])
+#bowlers= len(data2["bowling"]["bowler"])
 #data2=c.scorecard(1)
-print json.dumps(data2,indent=4)
-print "%s"%(data2["batting"]["score"][0]["overs"])
-if(matchinfo["type"] == "ODI" or matchinfo["type"] == "T20"):
-    c = Oneday(data2)
-    print c
+#print json.dumps(data2,indent=4)
+#print "%s"%(data2["batting"]["score"][0]["overs"])
+data=c.scorecard(15)
+print json.dumps(data, indent=4)
+for match in matches:
+    if(match["mchstate"] in ["preview","nextlive"]):
+        print preview(match)
+    elif((match["mchstate"]=="complete" or match["mchstate"] == "Result")):
+        print complete(match)
+    else:
+        data2=c.livescore(match["id"])
+        print livescore(data2)
