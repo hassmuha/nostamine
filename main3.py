af1ee44835aaafa09e92dd9bf07d8522d7c0d623
Muhammad Hassan <hassan.nust@gmail.com>
2017-02-07 19:39:18 +0100

    no message

--- main3.py
+++ main3.py
@@ -329,10 +329,85 @@ def send_alluser_result(token, date):
         print "debug"
         if post_match["result"] != "XX":
             print post_match["result"]
+
+            if post_match["result"] == "KK":
+                team_imgurl = "https://pslt20.blob.core.windows.net/team/1453111172542-team.png"
+                team_weburl = "http://match.psl-t20.com/teams/karachi-kings"
+                team_name = "Karachi Kings"
+            elif post_match["result"] == "IU":
+                team_imgurl = "https://pslt20.blob.core.windows.net/team/1453111086101-team.png"
+                team_weburl = "http://match.psl-t20.com/teams/islamabad-united"
+                team_name = "Islamabad United"
+            elif post_match["result"] == "LQ":
+                team_imgurl = "https://pslt20.blob.core.windows.net/team/1453111135284-team.png"
+                team_weburl = "http://match.psl-t20.com/teams/lahore-qalandars"
+                team_name = "Lahore Qalandars"
+            elif post_match["result"] == "QG":
+                team_imgurl = "https://pslt20.blob.core.windows.net/team/1453111043838-team.png"
+                team_weburl = "http://match.psl-t20.com/teams/quetta-gladiators"
+                team_name = "Quetta Gladiators"
+            else:
+                team_imgurl = "https://pslt20.blob.core.windows.net/team/1453111156446-team.png"
+                team_weburl = "http://match.psl-t20.com/teams/peshawar-zalmi"
+                team_name = "Peshawar Zalmi"
+
+            win_title = Congratulations!!
             for post_user in db_coluser.find({"fbID": {'$exists': True}}):
                 if {u'match': post_match["match"], u'bet': post_match["result"], u'date': date} in post_user["bets"]:
                     print "find result"
                     print post_user
+                    # update the score
+                    # send msg abt the winner
+                    win_subtitle = "%s wins on %s, your new score : %i" % (team_name,date,post_user["betrating"])
+                    murl = 'http://m.me/NostalMine?ref={0}'.format(post_user["fbID"])
+                    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=json.dumps({
+                        "recipient": {"id": post_user["fbID"]},
+                        "message": {
+                          "attachment":{
+                              "type":"template",
+                              "payload":{
+                                  "template_type":"generic",
+                                  "elements":[
+                                      {
+                                          "title":win_title,
+                                          "subtitle":win_subtitle,
+                                          "image_url":team_imgurl,
+                                          "buttons":[
+                                              {
+                                                  "type":"element_share"
+                                              },
+                                              {
+                                                  "type":"web_url",
+                                                  "url":murl,
+                                                  "title":"Challenge accepted"
+                                              }
+                                          ]
+                                      }
+                                  ]
+                              }
+                          }
+                        }
+                      }),
+                      headers={'Content-type': 'application/json'})
+                    if r.status_code != requests.codes.ok:
+                      print r.text
+                else:
+                    # send msg abt the lost
+                    lost_text = "Sorry you lost the bet, %s wins on %s, your score remain: %i" % (team_name,date,post_user["betrating"])
+                    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                      params={"access_token": token},
+                      data=json.dumps({
+                        "recipient": {"id": post_user["fbID"]},
+                        "message": {
+                          "text":lost_text
+                        }
+                      }),
+                      headers={'Content-type': 'application/json'})
+                    if r.status_code != requests.codes.ok:
+                      print r.text
+
     print "debug1"
 
 #match no is just for the allignment for iteratively send new match
