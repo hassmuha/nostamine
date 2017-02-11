eae5e50d3537b7b6a85ffd766fdd2c755b64ff7e
Muhammad Hassan <hassan.nust@gmail.com>
2017-02-11 23:14:28 +0100

    no message

--- main4.py
+++ main4.py
@@ -315,9 +315,7 @@ def addbet_dbcoluser(fbID,match,bet,date):
 
     pprint.pprint(post)
     post_bet = {"match":match,"bet":bet,"date":date}
-    print post["bets"]
-    print post_bet
-    if post_bet in post["bets"]:
+    if post_bet not in post["bets"]:
         db_coluser.update_one({"fbID": fbID},{"$push": { "bets" : post_bet}} )
         print "new entry added"
     else:
