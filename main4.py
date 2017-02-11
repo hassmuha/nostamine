2abec09e2ad7e362d1e5245c9db904477a186f1f
Muhammad Hassan <hassan.nust@gmail.com>
2017-02-11 16:42:29 +0100

    no message

--- main4.py
+++ main4.py
@@ -628,10 +628,11 @@ def send_currentmatch(token, recipient):
         [team1,team2] = matchinfo["match"].split(':')
         team1_name = team_map[team1]
         team2_name = team_map[team2]
+
         dt = datetime.datetime.now()
         currenttime = dt.hour * 60 + dt.minute
 
-        if("Match will start" in matchinfo["status"] and matchinfo["lastupdate"] < currenttime):
+        if("Match will start" in matchinfo["status"] and currenttime < matchinfo["lastupdate"]):
             data.append({"content_type":"text", "title":"%s vs %s" %(team1_name,team2_name), "payload":"GS_%i_preview"%(idx), "image_url": "http://www.cs.odu.edu/~rnagella/harris@nrk/reverse%20engineering/AWForms/res/drawable/icon_yellow_dot.png"})
         elif("won" in matchinfo["status"].lower() or "tied" in matchinfo["status"].lower()):
             data.append({"content_type":"text", "title":"%s vs %s" %(team1_name,team2_name), "payload":"GS_%i_complete"%(idx), "image_url": "https://cdn-img.easyicon.net/png/11744/1174475.gif"})
